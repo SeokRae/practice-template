407198bf1af3c0286665809d24e9ed669b77cc3e
seokrae <kslbsh@gmail.com>
2022-03-16 23:46:52 +0900

    Initial commit

---
+++ src/main/java/com/example/practicetemplate/PracticeTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicetemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeTemplateApplication.class, args);
+	}
+
+}
